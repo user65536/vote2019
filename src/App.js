cf91e14c6c176382cc1eebfc4d1cc8d3bc812af2
User65536 <mengxiangji365@163.com>
2019-02-15 17:51:21 +0800

    add iconfont.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
-import LazyLoad from './components/LazyLoad'
+import Login from './pages/Login';
+import Gallery from './pages/Gallery';
+import Detail from './pages/Detail';
 
-
+import './font/iconfont.css'
 import './index.styl'
+
 class App extends Component {
   render() {
     return (
-      <>
-        <div className="app">react app</div>
-        <div style={{width: "280px", height: "200px"}}>
-          <LazyLoad 
-          src="http://192.168.8.15:8000" 
-          alt="test" 
-          loadingSrc="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3257716873,281158601&fm=27&gp=0.jpg" 
-          >
-          </LazyLoad>
-        </div>
-        <img style={{width: "280px", height: "200px"}} src="http://192.168.8.15:8000" alt=""/>
-      </>
+      <Router>
+        <>
+          <Switch>
+            <Route path="/" exact component={Login}></Route>
+            <Route path="/login" component={Login}></Route>
+            <Route path="/gallery" component={Gallery}></Route>
+            <Route path="/detail" component={Detail}></Route>
+            {/* <Route path="/" component={NotFound}></Route> */}
+          </Switch>
+        </>
+      </Router>
     );
   }
 }
