d22c474c165088f409603143dd3db385946316c4
User65536 <mengxiangji365@163.com>
2019-02-25 21:59:58 +0800

    add component nav

---
+++ src/components/Nav.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+import '../styles/nav.styl'
+
+class c extends Component {
+
+  render () {
+    return (
+      <>
+      <div className="wrapper-nav">
+        <div className="logo"></div>
+        <div className="slot">{
+          this.props.children
+        }</div>
+        <div className="right">
+          <div className="ticker">
+            <span className="iconfont icon-toupiao"></span>
+            <span className="num">5</span>
+          </div>
+          <div className="user">2017210550</div>
+          <div className="quit">
+            <span className="iconfont icon-dengchu"></span>
+          </div>
+          
+        </div>
+      </div>
+      <div className="black"></div>
+      </>
+    )
+  }
+
+}
+
+export default c;
