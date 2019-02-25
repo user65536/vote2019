06c257474a053641ae049af44c0ef029b95e9968
User65536 <mengxiangji365@163.com>
2019-02-25 22:00:27 +0800

    gallery start not finish

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import ProjectCard from '../components/ProjectCard'
+import Nav from '../components/Nav'
 
 import '../styles/gallery.styl'
 
@@ -36,22 +37,72 @@ class Gallery extends Component {
         ],
         "vote": 10,
         "view": 900
+      },
+      {
+        "id": "3",
+        "name": "基于视频流的运动物",
+        "img": [
+          "http://img0.imgtn.bdimg.com/it/u=1664316807,3401349985&fm=26&gp=0.jpg",
+          "213123"
+        ],
+        "vote": 10,
+        "view": 900
+      },
+      {
+        "id": "3",
+        "name": "基于视频流的运动物",
+        "img": [
+          "http://img0.imgtn.bdimg.com/it/u=1664316807,3401349985&fm=26&gp=0.jpg",
+          "213123"
+        ],
+        "vote": 10,
+        "view": 900
+      },{
+        "id": "3",
+        "name": "基于视频流的运动物",
+        "img": [
+          "http://img0.imgtn.bdimg.com/it/u=1664316807,3401349985&fm=26&gp=0.jpg",
+          "213123"
+        ],
+        "vote": 10,
+        "view": 900
+      },
+      {
+        "id": "3",
+        "name": "基于视频流的运动物",
+        "img": [
+          "http://img0.imgtn.bdimg.com/it/u=1664316807,3401349985&fm=26&gp=0.jpg",
+          "213123"
+        ],
+        "vote": 10,
+        "view": 900
       }
     ]
   }
 
   render () {
     return (
-      <div className="wrap">
-        {
-          this.state.projects.map((ele, index) => {
-            return (
-              <div key={ele.id} className="card-wrap">
-                <ProjectCard info={ele}></ProjectCard>
-              </div>
-            )
-          })
-        }
+      <div className="wrapper-gallery">
+        <Nav>
+          <>
+            {/* <div className="group">
+              <span className="group-name">全部分组</span>
+              <span className="iconfont icon-optio3"></span>
+            </div> */}
+          </>
+        </Nav>
+        <div className="content">
+          {
+            this.state.projects.map((ele, index) => {
+              return (
+                <div key={ele.id} className="card-wrap">
+                  <ProjectCard info={ele}></ProjectCard>
+                </div>
+              )
+            })
+          }
+        </div>
+        
       </div>
     )
   }
