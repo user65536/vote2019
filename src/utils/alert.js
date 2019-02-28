5b33bfa4f1df9832d3c6e26b31f637d6148489ca
User65536 <mengxiangji365@163.com>
2019-02-28 18:42:36 +0800

    add js utils

---
+++ src/utils/alert.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let alert = (
+  <div className="alert">
+    弹出
+  </div>
+)
+
+ReactDOM.render(alert, document.getElementById('root'));
+
+
