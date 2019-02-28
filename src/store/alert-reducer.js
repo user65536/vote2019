aeb07acfad065075aa62a6e1a56772d412f78f99
User65536 <mengxiangji365@163.com>
2019-02-28 21:30:14 +0800

    install redux

---
+++ src/store/alert-reducer.js
@@ -0,0 +1,13 @@
+const defaultStore = {
+  text: ''
+}
+
+export default (store=defaultStore, action) => {
+  const newStore = JSON.parse(JSON.stringify(store))
+  switch(action.type) {
+    case 'SHOW_ALERT': 
+      newStore.text = action.value;
+      return newStore
+  }
+  return store
+}
