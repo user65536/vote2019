a1fb9202a29c017d9e2f60334e24c55b4c3a3e7c
User65536 <mengxiangji365@163.com>
2019-03-02 00:36:27 +0800

    add 2 api for login

--- src/utils/vote.js
+++ src/utils/vote.js
@@ -1,6 +1,40 @@
+import ajax from './_ajax'
+
 const host = "http://result.eolinker.com/Cd3uNIt2f3c1f80117d47fc916e8ce8d799404697481ae5?uri="
 
 const apiAddr = {
-  login: host + '/api/login'
+  login: '/api/login',
+  logout: '/api/logout',
+  checkLogin: '/api/checkLogin',
+}
+for (let [k, v] of Object.entries(apiAddr) ) {
+  apiAddr[k] = `${host}${v}`
+  apiAddr[`${k}_f`] = `${host}${v}&resultType=failure`
+}
+
+export default {
+  login ({username, password}) {
+    return new Promise((resolve, reject) => {
+      ajax({
+        url: apiAddr.login,
+        method: 'POST',
+        data: {username, password}
+      }).then(response => {
+        if (typeof response === 'string') {
+          reject('系统错误:JSON needed')
+        }
+        resolve(response)
+      }).catch(reject)
+    })
+  },
+  checkLogin() {
+    return new Promise((resolve, reject) => {
+      ajax({
+        url: apiAddr.checkLogin_f,
+        method: 'GET'
+      }).then(({loginState}) => {resolve(loginState)})
+        .catch(reject)
+    })
+  }
 }
 
