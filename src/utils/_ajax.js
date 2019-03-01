837519431cc4cbf611e46bf74f82b395969d690e
User65536 <mengxiangji365@163.com>
2019-03-02 00:36:10 +0800

    手动封装Promise Ajax函数

--- src/utils/_ajax.js
+++ src/utils/_ajax.js
@@ -13,48 +13,40 @@
  * responseType // 请求传入的数据
  */
 
-// 默认的ajax参数
-let ajaxDefaultOptions = {
-  url: '#', // 请求地址，默认为空
-  method: 'GET', // 请求方式，默认为GET请求
-  async: true, // 请求同步还是异步，默认异步
-  timeout: 0, // 请求的超时时间
-  dataType: 'text', // 请求的数据格式，默认为text
-  data: null, // 请求的参数，默认为空
-  headers: {}, // 请求头，默认为空
-  onprogress: function () {}, // 从服务器下载数据的回调
-  onuploadprogress: function () {}, // 处理上传文件到服务器的回调
-  xhr: null // 允许函数外部创建xhr传入，但是必须不能是使用过的
-};
 
-function _ajax(paramOptions) {
-  let options = {};
-  for (const key in ajaxDefaultOptions) {
-    options[key] = ajaxDefaultOptions[key];
-  }
-  // 如果传入的是否异步与默认值相同，就使用默认值，否则使用传入的参数
-  options.async = paramOptions.async === ajaxDefaultOptions.async ? ajaxDefaultOptions.async : paramOptions.async;
-  // 判断传入的method是否为GET或者POST,否则传入GET 或者可将判断写在promise内部，reject出去
-  options.method = paramOptions.method ? ("GET" || "POST") : "GET";
-  // 如果外部传入xhr，否则创建一个
-  let xhr = options.xhr || new XMLHttpRequest();
+function _ajax({
+  url='#',
+  method='GET',
+  async=true,
+  timeout=600,
+  dataType='text',
+  data=null,
+  headers={},
+  onprogress= () => {},
+  onuploadprogress= () => {},
+  xhr= new XMLHttpRequest()
+}) {
   // return promise对象
   return new Promise(function (resolve, reject) {
-    xhr.open(options.method, options.url, options.async);
-    xhr.timeout = options.timeout;
+    xhr.open(method, url, async);
+    xhr.timeout = timeout;
     // 设置请求头
-    for (const key in options.headers) {
-      xhr.setRequestHeader(key, options.headers[key]);
+    for (const key in headers) {
+      xhr.setRequestHeader(key, headers[key]);
     }
     // 注册xhr对象事件
-    xhr.responseType = options.dataType;
-    xhr.onprogress = options.onprogress;
-    xhr.onuploadprogress = options.onuploadprogress;
+    xhr.responseType = dataType;
+    xhr.onprogress = onprogress;
+    xhr.onuploadprogress = onuploadprogress;
     // 开始注册事件
     // 请求成功
     xhr.onloadend = function () {
       if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
-        resolve(xhr);
+        try {
+          resolve(JSON.parse(xhr.response))
+        } catch (e) {
+          resolve(xhr.response);
+        }
       } else {
         reject({
           errorType: "status_error",
@@ -85,7 +77,7 @@ function _ajax(paramOptions) {
     }
     // 捕获异常
     try {
-      xhr.send(options.data);
+      xhr.send(data);
     } catch (error) {
       reject({
         errorType: "send_error",
@@ -97,17 +89,19 @@ function _ajax(paramOptions) {
 
 
 // 调用示例
-_ajax({
-  url: 'http://localhost:3000/suc',
-  async: true,
-  onprogress: function (evt) {
-    console.log(evt.position / evt.total);
-  },
-  dataType: 'text/json'
-}).then(
-  function (xhr) {
-    console.log(xhr.response);
-  },
-  function (e) {
-    console.log(JSON.stringify(e))
-  });
+// _ajax({
+//   url: 'http://localhost:3000/suc',
+//   async: true,
+//   onprogress: function (evt) {
+//     console.log(evt.position / evt.total);
+//   },
+//   dataType: 'text/json'
+// }).then(
+//   function (xhr) {
+//     console.log(xhr.response);
+//   },
+//   function (e) {
+//     console.log(JSON.stringify(e))
+//   });
+
+export default _ajax
