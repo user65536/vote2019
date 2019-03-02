c0fe5f3c17364a63c74a147fa9c226f9f736b617
User65536 <mengxiangji365@163.com>
2019-03-02 17:58:01 +0800

    add src length count api

--- src/utils/page.js
+++ src/utils/page.js
@@ -7,5 +7,13 @@ export default {
       value: title
     }
     alertStore.dispatch(action)
+  },
+  getStrLength(str) {
+    const len = str.length
+    let charLength = 0
+    for(let i = 0; i < len; i ++) {
+      charLength += (str.charCodeAt(i) > 255 ? 1 : 0.5)
+    }
+    return charLength
   }
 }
