a073f1719ed586c50a739f0ca9942cf8cb63a83c
User65536 <mengxiangji365@163.com>
2019-03-03 00:12:05 +0800

    lock submit btn after clicking

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -8,7 +8,8 @@ class Toast extends Component {
   codeInput = React.createRef()
   state = {
     warnText: '投票后不可撤销或更改',
-    code: null
+    code: null,
+    submitLock: false
   }
   componentDidMount () {
     this.renderCode()
@@ -25,26 +26,33 @@ class Toast extends Component {
             <img className="code" src={this.state.code} alt="验证码"/>
           </div>
           <div className="warn-wrap">
-            <span className="iconfont icon-xianshi_jinggao"></span>
+            {this.state.warnText ? <span className="iconfont icon-xianshi_jinggao"></span> : ''}
             <span className="warn-text">{this.state.warnText}</span>
           </div>
           <div className="btn-wrap">
             <div className="btn cancel" onClick={this.props.onHide} >取消</div>
-            <div className="btn submit" onClick={this.submitCode}>提交</div>
+            <div className={`btn submit ${this.state.submitLock ? 'lock' : ''}`} onClick={this.submitCode}>{this.state.submitLock ? '提交中' : '提交'}</div>
           </div>
         </div>
       </>
     )
   }
   submitCode = () => {
+    if(this.state.submitLock) {
+      return false
+    } else {
+      this.submitLock("lock")
+    }
     const value = this.codeInput.current.value
     const id = this.props.id
     vote.voteNow({captcha: value, id})
     .then(res => {
       page.showAlert("投票成功")
+      this.submitLock("unlock")
       this.props.onHide()
     })
     .catch(err => {
+      this.submitLock("unlock")
       if(err === "验证码错误") {
         this.setState({
           code: null
@@ -61,6 +69,11 @@ class Toast extends Component {
         code: res
       })
     }).catch(page.showAlert)
+  }
+  submitLock = (operate) => {
+    this.setState({
+      submitLock: operate === 'lock'
+    })
   }
 }
 
