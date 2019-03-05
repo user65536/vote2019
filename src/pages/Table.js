fe5ae9d3ff4f9b1f5e1aefa4c6b8ccb0c05edca0
User65536 <mengxiangji365@163.com>
2019-03-05 20:13:01 +0800

    header styles

--- src/pages/Table.js
+++ src/pages/Table.js
@@ -1,73 +1,122 @@
+
 import React, { Component } from 'react';
 
 import '../styles/table.styl'
+import logo from '../static/logo2.png'
 
-import page from '../utils/page'
+import vote from '../utils/vote'
 
 class Table extends Component {
   state = {
-    list: [{"id":0,"name":"test项项项项项目0test项目0test项目0test项目0ss","voteNumber":20000},{"id":1,"name":"test项目1","voteNumber":18},{"id":2,"name":"test项目2","voteNumber":11},{"id":3,"name":"test项目3","voteNumber":6},{"id":4,"name":"test项目4","voteNumber":5},{"id":5,"name":"test项目5","voteNumber":4},{"id":6,"name":"test项目6","voteNumber":11},{"id":7,"name":"test项目7","voteNumber":19},{"id":8,"name":"test项目8","voteNumber":15},{"id":9,"name":"test项目9","voteNumber":16},{"id":10,"name":"test项目10","voteNumber":17},{"id":11,"name":"test项目11","voteNumber":7},{"id":12,"name":"test项目12","voteNumber":11},{"id":13,"name":"test项目13","voteNumber":15},{"id":14,"name":"test项目14","voteNumber":4},{"id":15,"name":"test项目15","voteNumber":3},{"id":16,"name":"test项目16","voteNumber":7},{"id":17,"name":"test项目17","voteNumber":15},{"id":18,"name":"test项目18","voteNumber":15},{"id":19,"name":"test项目19","voteNumber":2},{"id":20,"name":"test项目20","voteNumber":15},{"id":21,"name":"test项目21","voteNumber":18},{"id":22,"name":"test项目22","voteNumber":11},{"id":23,"name":"test项目23","voteNumber":7},{"id":24,"name":"test项目24","voteNumber":4},{"id":25,"name":"test项目25","voteNumber":11},{"id":26,"name":"test项目26","voteNumber":12},{"id":27,"name":"test项目27","voteNumber":1},{"id":28,"name":"test项目28","voteNumber":16},{"id":29,"name":"test项目29","voteNumber":17},{"id":30,"name":"test项目30","voteNumber":10},{"id":31,"name":"test项目31","voteNumber":15},{"id":32,"name":"test项目32","voteNumber":16},{"id":33,"name":"test项目33","voteNumber":13},{"id":34,"name":"test项目34","voteNumber":13},{"id":35,"name":"test项目35","voteNumber":9},{"id":36,"name":"test项目36","voteNumber":5},{"id":37,"name":"test项目37","voteNumber":11},{"id":38,"name":"test项目38","voteNumber":0},{"id":39,"name":"test项目39","voteNumber":10},{"id":40,"name":"test项目40","voteNumber":2},{"id":41,"name":"test项目41","voteNumber":8}],
+    list: '',
     currentGroup: 0,
     currentList: [],
     nextList: [],
     scroll: false
   }
   componentDidMount () {
-    this.setState({
-      currentList: this.state.list.slice(0, 12),
-      nextList: this.state.list.slice(12, 24)
+    this.addFnToWindow()
+    vote.getVoteRecord().then(list => {
+      this.setState({
+        list: list,
+        currentList: list.slice(0, 12),
+        nextList: list.slice(12, 24)
+      })
+    }).catch(() => {
+
     })
-    setInterval(() => {
-      // this.change()
-    }, 4000)
+    window.vote.timer = setInterval(() => {
+      this.change()
+    }, 5000)
   }
   render () {
     return (
-      <div className="wrapper-table">
-        <div className="header"></div>
-        <div className="content">
-          <div className="current-wrap">
-            {
-              this.state.currentList.map((project, index) => {
-                return (
-                  <div className={`item item-${index % 6 + 1} ${index > 5 ? 'item-right' : ''} ${this.state.scroll ? "previous" : "current"}`}>
-                    <div className="rank">{project.id + 1}</div>
-                    <div className={`title`}>
-                      <span>{project.name}</span>
-                    </div>
-                    <div className="voteNumber">{project.voteNumber}票</div>
-                  </div>
-                )
-              })
-            }
-            {/* <div className={`item item-2 ${this.state.scroll ? "previous" : "current"}`}></div>
-            <div className={`item item-3 ${this.state.scroll ? "previous" : "current"}`}></div>
-            <div className={`item item-4 ${this.state.scroll ? "previous" : "current"}`}></div>
-            <div className={`item item-5 ${this.state.scroll ? "previous" : "current"}`}></div>
-            <div className={`item item-6 ${this.state.scroll ? "previous" : "current"}`}></div>
-            <div className={`item item-right item-1 ${this.state.scroll ? "previous" : "current"}`}></div>
-            <div className={`item item-right item-2 ${this.state.scroll ? "previous" : "current"}`}></div>
-            <div className={`item item-right item-3 ${this.state.scroll ? "previous" : "current"}`}></div>
-            <div className={`item item-right item-4 ${this.state.scroll ? "previous" : "current"}`}></div>
-            <div className={`item item-right item-5 ${this.state.scroll ? "previous" : "current"}`}></div>
-            <div className={`item item-right item-6 ${this.state.scroll ? "previous" : "current"}`}></div> */}
+      <>
+        <div className="warn">屏幕宽度过窄</div>
+        <div className="wrapper-table">
+          <div className="header">
+            <div className="left">
+              <div className="logo">
+                <img src={logo} alt="第十一届大创展"/>
+              </div>
+              {/* <div className="title">
+                <div className="zh">北京邮电大学</div>
+                <div className="en">第十一届大创展</div>
+                <div className="en">实时得票统计</div>
+              </div> */}
+            </div>
+            <div className="center">
+              <span className="text">共</span>
+              <span className="number">19999</span>
+              <span className="text">票</span>
+            </div>
+            <div className="right">
+              <div className="time">18<span className="point">:</span>30<span className="point">:</span>22</div>
+              <div className="date">星期六 3-2-2019</div>
+            </div>
           </div>
-          <div>
-            <div className={`item item-1 ${this.state.scroll ? "current current-withTransition" : "next"}`}></div>
-            <div className={`item item-2 ${this.state.scroll ? "current current-withTransition" : "next"}`}></div>
-            <div className={`item item-3 ${this.state.scroll ? "current current-withTransition" : "next"}`}></div>
-            <div className={`item item-4 ${this.state.scroll ? "current current-withTransition" : "next"}`}></div>
-            <div className={`item item-5 ${this.state.scroll ? "current current-withTransition" : "next"}`}></div>
-            <div className={`item item-6 ${this.state.scroll ? "current current-withTransition" : "next"}`}></div>
-            <div className={`item item-right item-1 ${this.state.scroll ? "current current-withTransition" : "next"}`}></div>
-            <div className={`item item-right item-2 ${this.state.scroll ? "current current-withTransition" : "next"}`}></div>
-            <div className={`item item-right item-3 ${this.state.scroll ? "current current-withTransition" : "next"}`}></div>
-            <div className={`item item-right item-4 ${this.state.scroll ? "current current-withTransition" : "next"}`}></div>
-            <div className={`item item-right item-5 ${this.state.scroll ? "current current-withTransition" : "next"}`}></div>
-            <div onTransitionEnd={this.backToStart} className={`item item-right item-6 ${this.state.scroll ? "current current-withTransition" : "next"}`}></div>
+          <div className="content">
+            <div className="current-wrap">
+              {
+                this.state.currentList.map((project, index) => {
+                  return (
+                    <div key={project.id+project.name} className={`item item-${index % 6 + 1} ${index > 5 ? 'item-right' : ''} ${this.state.scroll ? "previous" : "current"}`}>
+                      <div className="rank">{project.rank}</div>
+                      <div className={`title`}>
+                        <span>{project.name}</span>
+                      </div>
+                      <div className="voteNumber">{project.voteNumber}票</div>
+                    </div>
+                  )
+                })
+              }
+              {/* <div className={`item item-2 ${this.state.scroll ? "previous" : "current"}`}></div>
+              <div className={`item item-3 ${this.state.scroll ? "previous" : "current"}`}></div>
+              <div className={`item item-4 ${this.state.scroll ? "previous" : "current"}`}></div>
+              <div className={`item item-5 ${this.state.scroll ? "previous" : "current"}`}></div>
+              <div className={`item item-6 ${this.state.scroll ? "previous" : "current"}`}></div>
+              <div className={`item item-right item-1 ${this.state.scroll ? "previous" : "current"}`}></div>
+              <div className={`item item-right item-2 ${this.state.scroll ? "previous" : "current"}`}></div>
+              <div className={`item item-right item-3 ${this.state.scroll ? "previous" : "current"}`}></div>
+              <div className={`item item-right item-4 ${this.state.scroll ? "previous" : "current"}`}></div>
+              <div className={`item item-right item-5 ${this.state.scroll ? "previous" : "current"}`}></div>
+              <div className={`item item-right item-6 ${this.state.scroll ? "previous" : "current"}`}></div> */}
+            </div>
+            <div>
+              {
+                this.state.nextList.map((project, index) => {
+                  return (
+                    <div 
+                    onTransitionEnd={(this.state.nextList.length > 6 ? 
+                    (index === 5 ? this.backToStart : null ) : 
+                    index === this.state.nextList.length - 1 ? this.backToStart : null) } 
+                    key={project.id + project.name + 'next'} 
+                    className={`item item-${index % 6 + 1} ${index > 5 ? 'item-right' : ''}  ${this.state.scroll ? "current current-withTransition" : "next"}`}>
+                      <div className="rank">{project.rank}</div>
+                      <div className={`title`}>
+                        <span>{project.name}</span>
+                      </div>
+                      <div className="voteNumber">{project.voteNumber}票</div>
+                    </div>
+                  )
+                })
+              }
+              {/* <div className={`item item-1 ${this.state.scroll ? "current current-withTransition" : "next"}`}></div>
+              <div className={`item item-2 ${this.state.scroll ? "current current-withTransition" : "next"}`}></div>
+              <div className={`item item-3 ${this.state.scroll ? "current current-withTransition" : "next"}`}></div>
+              <div className={`item item-4 ${this.state.scroll ? "current current-withTransition" : "next"}`}></div>
+              <div className={`item item-5 ${this.state.scroll ? "current current-withTransition" : "next"}`}></div>
+              <div className={`item item-6 ${this.state.scroll ? "current current-withTransition" : "next"}`}></div>
+              <div className={`item item-right item-1 ${this.state.scroll ? "current current-withTransition" : "next"}`}></div>
+              <div className={`item item-right item-2 ${this.state.scroll ? "current current-withTransition" : "next"}`}></div>
+              <div className={`item item-right item-3 ${this.state.scroll ? "current current-withTransition" : "next"}`}></div>
+              <div className={`item item-right item-4 ${this.state.scroll ? "current current-withTransition" : "next"}`}></div>
+              <div className={`item item-right item-5 ${this.state.scroll ? "current current-withTransition" : "next"}`}></div>
+              <div onTransitionEnd={this.backToStart} className={`item item-right item-6 ${this.state.scroll ? "current current-withTransition" : "next"}`}></div> */}
+            </div>
           </div>
         </div>
-      </div>
+      </>
     )
   }
   change = () => {
@@ -76,9 +125,35 @@ class Table extends Component {
     })
   }
   backToStart = () => {
+    let groupLength = Math.ceil(this.state.list.length / 12)
+    let currentGroup = (this.state.currentGroup + 1) % groupLength
+    let nextGroup = (this.state.currentGroup + 2) % groupLength
+    if(nextGroup === groupLength - 1) {
+      vote.getVoteRecord().then(list => {
+        this.setState({
+          list: list
+        })
+      })
+    }
     this.setState({
-      scroll: false
+      scroll: false,
+      currentGroup: currentGroup,
+      currentList: this.state.list.slice(currentGroup * 12, currentGroup * 12 + 12),
+      nextList: this.state.list.slice(nextGroup * 12, nextGroup * 12 + 12)
     })
+  }
+  addFnToWindow = () => {
+    window.vote = {}
+    window.vote.timer = null
+    window.vote.stopScroll = () => {
+      clearInterval(window.vote.timer)
+    }
+    window.vote.startScroll = (time) => {
+      clearInterval(window.vote.timer)
+      window.vote.timer = setInterval(() => {
+        this.change()
+      }, time)
+    }
   }
 }
 
