7f48ba6687bbbab2a4067f7d68bda319c133736c
User65536 <mengxiangji365@163.com>
2019-03-14 17:10:18 +0800

    add build folder

---
+++ build/precache-manifest.45dabb98a23fba90bc1c7af9558cb986.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "05d1f111a3e7b46ceda07e5879c972c1",
+    "url": "./static/media/digital.05d1f111.ttf"
+  },
+  {
+    "revision": "9937a04bea6d8db0b7a0",
+    "url": "./static/css/main.829eef64.chunk.css"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "1f4645da7ed0eed28ce5",
+    "url": "./static/js/2.1f4645da.chunk.js"
+  },
+  {
+    "revision": "cb987a22aff6a916075b89dc5e5b2348",
+    "url": "./static/media/ground.cb987a22.jpg"
+  },
+  {
+    "revision": "ad1bcdde81854d8f39a7f3a8ee03138c",
+    "url": "./static/media/logo2.ad1bcdde.png"
+  },
+  {
+    "revision": "9937a04bea6d8db0b7a0",
+    "url": "./static/js/main.9937a04b.chunk.js"
+  },
+  {
+    "revision": "820bcda609a69b4222e1a26f4dfdc661",
+    "url": "./static/media/yuan.820bcda6.ttf"
+  },
+  {
+    "revision": "5e97b077ffb5ae3be780d0d9a6e58fe9",
+    "url": "./static/media/iconfont.5e97b077.ttf"
+  },
+  {
+    "revision": "2c9245c57390dceaeeddb7c44a9586a7",
+    "url": "./static/media/iconfont.2c9245c5.woff"
+  },
+  {
+    "revision": "48946fc3aed902c56fde5c50a7f15fa2",
+    "url": "./static/media/iconfont.48946fc3.eot"
+  },
+  {
+    "revision": "fe57db44bc49a8b98790140b48bde977",
+    "url": "./static/media/iconfont.fe57db44.svg"
+  },
+  {
+    "revision": "e0c38f56368c072d165c95afbb243166",
+    "url": "./index.html"
+  }
+];
