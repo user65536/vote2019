5f395879e19be49f141be0aea9ef86e4d282ad08
User65536 <mengxiangji365@163.com>
2019-03-25 09:01:05 +0800

    gh-pages

---
+++ precache-manifest.719bf3a36bc4271c528f50a9a43ec3bf.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "05d1f111a3e7b46ceda07e5879c972c1",
+    "url": "./static/media/digital.05d1f111.ttf"
+  },
+  {
+    "revision": "a069df71524989011e56",
+    "url": "./static/css/main.80343253.chunk.css"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "101e326b167cfdbaa2ad",
+    "url": "./static/js/2.101e326b.chunk.js"
+  },
+  {
+    "revision": "1394dc48be2de74e2a2017c5d5c2bc42",
+    "url": "./static/media/bupt5.1394dc48.jpg"
+  },
+  {
+    "revision": "cb987a22aff6a916075b89dc5e5b2348",
+    "url": "./static/media/ground.cb987a22.jpg"
+  },
+  {
+    "revision": "ad1bcdde81854d8f39a7f3a8ee03138c",
+    "url": "./static/media/logo2.ad1bcdde.png"
+  },
+  {
+    "revision": "a069df71524989011e56",
+    "url": "./static/js/main.a069df71.chunk.js"
+  },
+  {
+    "revision": "820bcda609a69b4222e1a26f4dfdc661",
+    "url": "./static/media/yuan.820bcda6.ttf"
+  },
+  {
+    "revision": "7b19b6adaf2184d009b7ec70e9b1f858",
+    "url": "./static/media/iconfont.7b19b6ad.eot"
+  },
+  {
+    "revision": "2c44fc4759c1670e4d58970a157dbf11",
+    "url": "./static/media/iconfont.2c44fc47.woff"
+  },
+  {
+    "revision": "395bc1d381f51f069eabc9a16af5b1c1",
+    "url": "./static/media/iconfont.395bc1d3.ttf"
+  },
+  {
+    "revision": "981bc37aef3b969dff074debc542d949",
+    "url": "./static/media/iconfont.981bc37a.svg"
+  },
+  {
+    "revision": "231945e0840338722137eaca612ced6d",
+    "url": "./index.html"
+  }
+];
--- src/utils/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-  mock: true
-}
-
-export default config
